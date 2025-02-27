3984d672c2551295eff10470a8590dc72a643ee6
ghollamsimo <ghollamsimo1@gmail.com>
2025-02-27 23:49:27 +0000

    Feat --> Adding provider from (react-redux) and calling store file

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
+import {store} from "./redux/Store.ts";
+import {Provider} from "react-redux";
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 )
